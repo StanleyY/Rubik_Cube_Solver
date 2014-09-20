f571850de0fc77e5148121d2f61b1649363786ec
Stanley Yang <stanley.yang.sy@gmail.com>
2014-09-20 00:27:15 -0400

    working input

---
+++ rubik/Solvable.java
@@ -0,0 +1,30 @@
+package rubik;
+
+import java.util.Arrays; //REMOVE THIS SHIT
+import java.nio.file.*;
+import java.io.IOException;
+
+class Solvable {
+
+  static String readFile(String file_name) {
+    String cwd = System.getProperty("user.dir");
+    try {
+      byte[] content = Files.readAllBytes(Paths.get(cwd, "\\rubik", file_name));
+      return new String(content);
+    }
+    catch (IOException e){
+      System.out.println(e.toString());
+    }
+    return null;
+  }
+
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Please Input Filename.");
+      System.exit(1);
+    }
+    String file_name = args[0];
+    String input = readFile(file_name);
+    System.out.println(input);
+  }
+}
