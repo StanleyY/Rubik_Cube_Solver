dc35d4f0efaba5ae729d23fbdd9567e49d951817
Stanley Yang <stanley.yang.sy@gmail.com>
2014-10-19 01:54:49 -0400

    basic binary file

---
+++ rubik/GenerateCorners.java
@@ -0,0 +1,27 @@
+package rubik;
+
+import java.util.Arrays;
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
+
+class GenerateCorners {
+
+  //static byte left = (byte)240;
+  //static byte right = (byte)15;
+  //static byte[] values = new byte [44089920];
+  static byte[] values = new byte [50];
+
+  public static void main(String[] args){
+    try {
+     FileOutputStream output = new FileOutputStream("CornersValues");
+     values[0] = (byte)205;
+     values[1] = (byte)255;
+     output.write(values);
+     output.close();
+    } catch (java.io.IOException e) {
+     e.printStackTrace();
+     System.exit(1);
+    }
+  }
+}
