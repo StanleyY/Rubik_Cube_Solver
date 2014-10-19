d6505f70383279845697c65a230243fd9de367b3
Stanley Yang <stanley.yang.sy@gmail.com>
2014-10-19 03:06:35 -0400

    starting refactoring into a single cube class

---
+++ rubik/Cube.java
@@ -0,0 +1,163 @@
+package rubik;
+
+import java.util.Arrays;
+import java.nio.file.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+class Cube {
+/*
+R = 0
+G = 1
+Y = 2
+B = 3
+O = 4
+W = 5
+
+*/
+
+  public char[][] cube;
+  public int[] corners;
+  public int[] edges;
+
+  public Cube(char[] input){
+    this.cube = this.generateCube(input);
+    this.corners = this.getCorners(this.cube);
+    this.corners = this.getEdges(this.cube);
+  }
+
+  public char[][] generateCube(char[] input){
+    char[][] output = new char[6][9];
+    int index = 0;
+    int j;
+    int x = 0;
+    for (int i = 0; i < 6; i++){
+      j = 0;
+      if (i < 1 || i > 3) {
+        for (; j < 9; j++) {
+          output[i][j] = input[index];
+          index++;
+        }
+      }
+      else{
+        for (j = x; j < x+3; j++){
+          output[i][j] = input[index];
+          index++;
+        }
+        if(i > 2 && index < 36){
+          i = 0;
+          x += 3;
+        }
+      }
+    }
+    return output;
+  }
+
+
+  public void printCube(char[][] input) {
+  // Prints the cube in a more human readable format.
+  int i = 0;
+  while (i < 9){
+    if(i % 3 == 0) System.out.printf("\n   ");
+    System.out.printf("%c", input[0][i]);
+    i++;
+  }
+
+  i = 1;
+  int j = 0;
+  while (j < 9){
+    System.out.printf("\n");
+    while (i < 4){
+      System.out.printf("%c", input[i][j]);
+      j++;
+      if (j % 3 == 0) {
+        j = j - 3;
+        i++;
+      }
+    }
+    j=j+3;
+    i = 1;
+  }
+
+  for (int x: new int[]{4, 5}){
+    i = 0;
+    for (char c : input[x]){
+      if(i % 3 == 0) System.out.printf("\n   ");
+      System.out.printf("%c", c);
+      i++;
+    }
+  }
+  System.out.printf("\n\n");
+}
+
+
+  private int[] getCorners(char[][] cube) {
+    List<String> keys = Arrays.asList("GRW", "BRW", "GRY", "BRY", "GOY", "BOY", "GOW", "BOW");
+    int[] output = new int[8];
+
+    char[][] corners = new char[][]{
+      {cube[0][0], cube[1][0], cube[5][6]},
+      {cube[0][2], cube[5][8], cube[3][2]},
+      {cube[0][6], cube[2][0], cube[1][2]},
+      {cube[0][8], cube[3][0], cube[2][2]},
+
+      {cube[4][0], cube[1][8], cube[2][6]},
+      {cube[4][2], cube[2][8], cube[3][6]},
+      {cube[4][6], cube[5][0], cube[1][6]},
+      {cube[4][8], cube[3][8], cube[5][2]}
+    };
+
+    if (!checkStickers(corners)) return null;
+
+    String temp = "";
+    for (int i = 0; i < 8; i++){
+      Arrays.sort(corners[i]);
+      temp = new String(corners[i]);
+      output[i] = keys.indexOf(temp);
+    }
+
+    return output;
+  }
+
+  // Relic of prerefactoring. Can't be removed elegantly unfortunately
+  private boolean checkStickers(char[][] corners){
+    List<String> invalid = Arrays.asList("RWG", "RBW", "RGY", "RYB", "OYG", "OBY", "OGW", "OWB");
+    for (char[] c: corners){
+      if (invalid.indexOf(new String(c)) != -1){
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  private int[] getEdges(char[][] cube){
+    List<String> keys = Arrays.asList("RW", "GR", "BR", "RY", "GW", "GY", "BY", "BW", "OY", "GO", "BO", "OW");
+    int[] output = new int[12];
+
+    char[][] edges = new char[][]{
+      {cube[0][1], cube[5][7]}, {cube[0][3], cube[1][1]}, {cube[0][5], cube[3][1]}, {cube[0][7], cube[2][1]},
+      {cube[1][3], cube[5][3]}, {cube[1][5], cube[2][3]}, {cube[2][5], cube[3][3]}, {cube[3][5], cube[5][5]},
+      {cube[4][1], cube[2][7]}, {cube[4][3], cube[1][7]}, {cube[4][5], cube[3][7]}, {cube[4][7], cube[5][1]}
+    };
+
+    String temp = "";
+    for (int i = 0; i < 12; i++){
+      Arrays.sort(edges[i]);
+      temp = new String(edges[i]);
+      output[i] = keys.indexOf(temp);
+    }
+
+    return output;
+  }
+
+
+  public int sideFace(int face, int index){
+    if (index == 3) return 1;
+    if (index == 5) return 3;
+    if (index == 1) return (face == 2) ? 0 : 4;
+    else return (face == 2) ? 4 : 0;
+  }
+
+}
--- rubik/GenerateCorners.java
+++ rubik/GenerateCorners.java
@@ -7,12 +7,22 @@ import java.util.*;
 
 class GenerateCorners {
 
-  //static byte left = (byte)240;
-  //static byte right = (byte)15;
+  static byte left = (byte)240;
+  static byte right = (byte)15;
   //static byte[] values = new byte [44089920];
   static byte[] values = new byte [50];
 
+  static void errorCheck(byte[] values){
+    byte x = (byte)15;
+    for(int i = 0; i < values.length; i++){
+      if((values[i] & right) == x || ((values[i] & left) >> 4) == x) {
+        System.out.println("VALUE UNFILLED AT INDEX " + i);
+      }
+    }
+  }
+
   public static void main(String[] args){
+    Solvable.generateCube(Solvable.GOAL_STATE);
     try {
      FileOutputStream output = new FileOutputStream("CornerValues");
      values[0] = (byte)205;
